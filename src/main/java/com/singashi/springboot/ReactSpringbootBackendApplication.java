92b8c2875e1bacf1c6bb4a54292065f47ae2c772
Ashu <ashu.ask.singh@gmail.com>
2022-07-12 23:21:50 +0530

    add more codes

--- src/main/java/com/singashi/springboot/ReactSpringbootBackendApplication.java
+++ src/main/java/com/singashi/springboot/ReactSpringbootBackendApplication.java
@@ -1,13 +1,26 @@
 package com.singashi.springboot;
 
+import com.singashi.springboot.model.User;
+import com.singashi.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReactSpringbootBackendApplication {
+public class ReactSpringbootBackendApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactSpringbootBackendApplication.class, args);
 	}
 
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		this.userRepository.save(new User("Ashu", "Singh" ,"ashu@123.com"));
+		this.userRepository.save(new User("Akash", "Singh" ,"akash@123.com"));
+		this.userRepository.save(new User("Anu", "Singh" ,"anu@123.com"));
+	}
 }
--- src/main/java/com/singashi/springboot/model/User.java
+++ src/main/java/com/singashi/springboot/model/User.java
@@ -1,4 +1,34 @@
 package com.singashi.springboot.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 }
